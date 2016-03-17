7842d66f400af9a97255c4033676cf0291c4038b
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2016-03-17 16:00:30 +0000

    Add 'report a problem' link to footer.

--- wp-content/themes/taxcentreofexcellence/templates/footer.php
+++ wp-content/themes/taxcentreofexcellence/templates/footer.php
@@ -1,13 +1,10 @@
 <footer class="group js-footer" id="footer" role="contentinfo">
-
   <div class="footer-wrapper">
-
-
+    <a href="#" class="report-problem-link email-link">Report a problem </a>
     <div class="footer-meta">
       <div class="footer-meta-inner">
         Built by <a href="https://mojdigital.blog.gov.uk/">MOJ Digital</a>
       </div>
-
       <div class="copyright">
         <a href="http://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/copyright-and-re-use/crown-copyright/">© Crown copyright</a>
       </div>
