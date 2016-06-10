bcfa371bd3d293b8bbe4b72a972129285ee5f36f
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2016-06-10 16:21:59 +0100

    Create new ACF flexible content fields for the homepage, and WP-CLI command to migrate content from the old ACF fields.

---
+++ wp-content/themes/taxcentreofexcellence/lib/commands.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Roots\Sage\Commands;
+
+// WP_CLI class only exists when running via the command line
+if (!class_exists('\WP_CLI')) {
+  return;
+}
+
+use WP_CLI;
+
+$commands = array(
+  'Migrate_Homepage_Fields_To_Flexible_Content',
+);
+
+/**
+ * Create a command name slug from the supplied class name.
+ * 'Example_Command' will become 'example-command'
+ *
+ * @param string $class_name
+ * @return string
+ */
+function slug_command_name($class_name) {
+  $name = strtolower($class_name);
+  $name = str_replace('_', '-', $name);
+  return $name;
+}
+
+// Register commands from $commands array
+foreach ($commands as $command) {
+  $command_name = slug_command_name($command);
+  $class_name = __NAMESPACE__ . '\\' . $command;
+
+  require 'commands/' . $command . '.php';
+  WP_CLI::add_command($command_name, $class_name);
+}
