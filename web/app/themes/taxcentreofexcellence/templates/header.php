bd86262524e9ae26473901f730554b4dba3376e7
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2019-03-29 15:28:40 +0000

    Add GTM for click monitoring

--- web/app/themes/taxcentreofexcellence/templates/header.php
+++ web/app/themes/taxcentreofexcellence/templates/header.php
@@ -1,3 +1,7 @@
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8LPRJ7" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <div id="skiplink-container">
   <div>
     <a href="#content" class="skiplink">Skip to Content</a>
