90006a855c04079aabe6b50b3ea4ad11b6a3d015
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2019-04-01 10:44:19 +0100

    Remove GTM from files, default use plugin: GADFWP

--- web/app/themes/taxcentreofexcellence/templates/head.php
+++ web/app/themes/taxcentreofexcellence/templates/head.php
@@ -1,7 +1,4 @@
 <head>
-  <!-- Google Tag Manager -->
-  <script>!function(e,t,a,n,g){e[n]=e[n]||[],e[n].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});var m=t.getElementsByTagName(a)[0],r=t.createElement(a);r.async=!0,r.src="https://www.googletagmanager.com/gtm.js?id=GTM-K8LPRJ7",m.parentNode.insertBefore(r,m)}(window,document,"script","dataLayer");</script>
-  <!-- End Google Tag Manager -->
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
