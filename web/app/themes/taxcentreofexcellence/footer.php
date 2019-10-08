2f7cfedaa7cb3ceaab675a96bf4c78395e3ac10f
Adam <adam.brown@digital.justice.gov.uk>
2019-10-08 17:06:52 +0100

    Add new header footer files to theme root

---
+++ web/app/themes/taxcentreofexcellence/footer.php
@@ -0,0 +1,14 @@
+<footer class="group js-footer" id="footer" role="contentinfo">
+  <div class="footer-wrapper">
+    <p><a href="mailto:taxation@justice.gsi.gov.uk" class="report-problem-link email-link">Report a problem </a><br></p>
+    <p><a href="mailto:taxation@justice.gov.uk" class="report-problem-link email-link">Submit your feedback </a></p>
+    <div class="footer-meta">
+      <div class="footer-meta-inner">
+        Built by <a href="https://mojdigital.blog.gov.uk/">MOJ Digital</a>
+      </div>
+      <div class="copyright">
+        <a href="http://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/copyright-and-re-use/crown-copyright/">© Crown copyright</a>
+      </div>
+    </div>
+  </div>
+</footer>
